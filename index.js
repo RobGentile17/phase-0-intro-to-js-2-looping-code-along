1f7a3f1452d705b5c2ebc2b30a33aed40f507fee
Rob <robert.gentile@powerhrg.com>
2021-05-25 09:49:24 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards( namesArray, event ) {
+    let thankYou = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYou.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYou
+}
+
+function countDown( num ) {
+    while ( num > -1 ) {
+      console.log( num --);
+    
+    }
+  }
